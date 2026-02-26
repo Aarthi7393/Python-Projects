183318fb2b4d77abe6490f987040c8da31701a68
Aarthi7393 <aarthimcbe@gmail.com>
2026-02-26 20:49:52 +0100

    feat(day-26) : natoalphabet:updated

--- Day26_NATOAlphabet/Day26_NATOAlphabet.py
+++ Day26_NATOAlphabet/Day26_NATOAlphabet.py
@@ -4,10 +4,16 @@ import pandas as pd
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter :row.code for (index, row) in data.iterrows()}
 print(nato_alphabet)
+def generate_phonetic():
+    user_name = input("Enter your name: ").upper()
+    # letters = [ letter for letter in user_name ]
+    # print(letters)
+    try:
+        nato_phonetic_alphabet = [nato_alphabet[letter] for letter in user_name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(nato_phonetic_alphabet)
 
-user_name = input("Enter your name: ").upper()
-# letters = [ letter for letter in user_name ]
-# print(letters)
-nato_phonetic_alphabet = [nato_alphabet[letter] for letter in user_name]
-print(nato_phonetic_alphabet)
-
+generate_phonetic()
